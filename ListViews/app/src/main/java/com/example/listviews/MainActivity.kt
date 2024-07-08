58ace172c2073c4ab05590a4806d42d043db7284
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-07-08 16:30:17 +0530

    Introduction to ListView

---
+++ ListViews/app/src/main/java/com/example/listviews/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.listviews
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val listview : ListView = findViewById(R.id.lst)
+
+        val listOfNames = listOf<String>("A","B","C","D","E")
+        val Namesadapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1,listOfNames)
+        listview.adapter = Namesadapter
+    }
+}
