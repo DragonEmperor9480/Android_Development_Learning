127d82e3aba65ae90b4ba6b1706dcb1702891381
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-07-09 10:22:39 +0530

    more about listview

--- ListViews/app/src/main/java/com/example/listviews/MainActivity.kt
+++ ListViews/app/src/main/java/com/example/listviews/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.listviews
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,10 +22,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val listview : ListView = findViewById(R.id.lst)
+        val listview: ListView = findViewById(R.id.lst)
 
-        val listOfNames = listOf<String>("A","B","C","D","E")
-        val Namesadapter = ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1,listOfNames)
+        val listOfNames = listOf<String>("A", "B", "C", "D", "E")
+        val Namesadapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listOfNames)
         listview.adapter = Namesadapter
+
+        listview.setOnItemClickListener(object : OnItemClickListener {
+            override fun onItemClick(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val nameclicked = listOfNames[position]
+                Toast.makeText(this@MainActivity, nameclicked, Toast.LENGTH_SHORT).show()
+            }
+
+
+        })
     }
 }
