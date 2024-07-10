8b56cd060df5ba895de731de6d588e55c3d8255a
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-07-10 16:59:01 +0530

    Implementing Bottom Navigation bar with Fragments

--- Fragments/app/src/main/java/com/example/fragments/MainActivity.kt
+++ Fragments/app/src/main/java/com/example/fragments/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.fragments
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,15 +20,41 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        openFragment(FirstFragment())
+        val bottom_nav : BottomNavigationView = findViewById(R.id.bottom_nav)
 
-        val btn : Button = findViewById(R.id.btn)
-
-        btn.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fcv, FirstFragment())
-                .commit()
-
+        bottom_nav.setOnNavigationItemSelectedListener {
+            val id = it.itemId
+            when(id)
+            {
+                R.id.item_home->{
+                    openFragment(FirstFragment())
+                }
+                R.id.item_profile->{
+                    openFragment(SecondFragment())
+                }
+                R.id.item_cart->{
+                    openFragment(ThirdFragment())
+                }
+            }
+            return@setOnNavigationItemSelectedListener true
         }
 
+//        val btn : Button = findViewById(R.id.btn)
+//
+//        btn.setOnClickListener {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.fcv, FirstFragment())
+//                .commit()
+//
+//        }
+
+    }
+
+    private  fun openFragment(fragment: Fragment)
+    {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv,fragment)
+            .commit()
     }
 }
